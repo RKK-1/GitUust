4b583d0bfbfcf84fafab64b52ce1a4b4ac4e4cc6
Роман Ковешников <roman.k.g6@gmail.com>
2025-10-13 13:53:43 +0500

    5 Практика

--- Python/model.py
+++ Python/model.py
@@ -1,16 +1,33 @@
 from pydantic import BaseModel
-# class Item(BaseModel):
-#     item:str
-#     status:str
+from typing import List
+class Item(BaseModel):
+    item:str
+    status:str
 
 class Todo(BaseModel):
     id:int
     item:str
-
+class TodoItem(BaseModel):
+    item:str
     class Config:
-        Schema_extra = {
-            "Example": {
-                "id": 1,
-                "item": "Example schema!"
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
              }
     }
+        
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
