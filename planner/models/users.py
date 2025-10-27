ec7d0903bf11c6f66a88fd8b87e683e9117a8b86
Роман Ковешников <roman.k.g6@gmail.com>
2025-10-27 14:15:59 +0500

    Практика 7

---
+++ planner/models/users.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]] = []
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": []
+            }
+        }
+
+class NewUser(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]] = []
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": []
+            }
+        }
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!"
+            }
+        }
